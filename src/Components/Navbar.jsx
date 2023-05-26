3179122da63cf8d747afc37acd82e1b1c99ccc89
Dheeraj malik <Dheerajmlk123@gmail.com>
2023-05-26 23:38:27 +0530

    update

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -123,22 +123,22 @@ export default function Navbar() {
         </header>
 
         <div
-          className="site-blocks-cover"
+          className ="site-blocks-cover"
           style={{ backgroundImage: "url(images/hero_1.jpg)" }}
           data-aos="fade"
         >
-          <div className="container">
-            <div className="row align-items-start align-items-md-center justify-content-end">
-              <div className="col-md-5 text-center text-md-left pt-5 pt-md-0">
-                <h1 className="mb-2">Finding Your Perfect Shoes</h1>
-                <div className="intro-text text-center text-md-left">
-                  <p className="mb-4">
+          <div className ="container">
+            <div className ="row align-items-start align-items-md-center justify-content-end">
+              <div className ="col-md-5 text-center text-md-left pt-5 pt-md-0">
+                <h1 className ="mb-2">Finding Your Perfect Shoes</h1>
+                <div className ="intro-text text-center text-md-left">
+                  <p className ="mb-4">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                     Phasellus at iaculis quam. Integer accumsan tincidunt
                     fringilla.{" "}
                   </p>
                   <p>
-                    <a href="#" className="btn btn-sm btn-primary">
+                    <a href="#" className ="btn btn-sm btn-primary">
                       Shop Now
                     </a>
                   </p>
@@ -148,19 +148,19 @@ export default function Navbar() {
           </div>
         </div>
 
-        <div className="site-section site-section-sm site-blocks-1">
-          <div className="container">
-            <div className="row">
+        <div className ="site-section site-section-sm site-blocks-1">
+          <div className ="container">
+            <div className ="row">
               <div
-                className="col-md-6 col-lg-4 d-lg-flex mb-4 mb-lg-0 pl-4"
+                className ="col-md-6 col-lg-4 d-lg-flex mb-4 mb-lg-0 pl-4"
                 data-aos="fade-up"
                 data-aos-delay=""
               >
-                <div className="icon mr-4 align-self-start">
-                  <span className="icon-truck"></span>
+                <div className ="icon mr-4 align-self-start">
+                  <span className ="icon-truck"></span>
                 </div>
-                <div className="text">
-                  <h2 className="text-uppercase">Free Shipping</h2>
+                <div className ="text">
+                  <h2 className ="text-uppercase">Free Shipping</h2>
                   <p>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                     Phasellus at iaculis quam. Integer accumsan tincidunt
@@ -169,15 +169,15 @@ export default function Navbar() {
                 </div>
               </div>
               <div
-                className="col-md-6 col-lg-4 d-lg-flex mb-4 mb-lg-0 pl-4"
+                className ="col-md-6 col-lg-4 d-lg-flex mb-4 mb-lg-0 pl-4"
                 data-aos="fade-up"
                 data-aos-delay="100"
               >
-                <div className="icon mr-4 align-self-start">
-                  <span className="icon-refresh2"></span>
+                <div className ="icon mr-4 align-self-start">
+                  <span className ="icon-refresh2"></span>
                 </div>
-                <div className="text">
-                  <h2 className="text-uppercase">Free Returns</h2>
+                <div className ="text">
+                  <h2 className ="text-uppercase">Free Returns</h2>
                   <p>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                     Phasellus at iaculis quam. Integer accumsan tincidunt
@@ -186,15 +186,15 @@ export default function Navbar() {
                 </div>
               </div>
               <div
-                className="col-md-6 col-lg-4 d-lg-flex mb-4 mb-lg-0 pl-4"
+                className ="col-md-6 col-lg-4 d-lg-flex mb-4 mb-lg-0 pl-4"
                 data-aos="fade-up"
                 data-aos-delay="200"
               >
-                <div className="icon mr-4 align-self-start">
-                  <span className="icon-help"></span>
+                <div className ="icon mr-4 align-self-start">
+                  <span className ="icon-help"></span>
                 </div>
-                <div className="text">
-                  <h2 className="text-uppercase">Customer Support</h2>
+                <div className ="text">
+                  <h2 className ="text-uppercase">Customer Support</h2>
                   <p>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                     Phasellus at iaculis quam. Integer accumsan tincidunt
@@ -231,11 +231,7 @@ export default function Navbar() {
               >
                 <a className="block-2-item" href="#">
                   <figure className="image">
-                    <img
-                      src="images/children.jpg"
-                      alt=""
-                      className="img-fluid"
-                    />
+                    <img src="images/children.jpg" alt="" className="img-fluid" />
                   </figure>
                   <div className="text">
                     <span className="text-uppercase">Collections</span>
