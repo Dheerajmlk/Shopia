c51faa97a19bc4513e4aa0c666262592ecac47a5
Dheeraj malik <Dheerajmlk123@gmail.com>
2023-06-03 14:36:36 +0530

    create pages

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,12 +1,26 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./Navbar";
+import Home from "./Home";
+import Footer from "./Footer";
+import Shop from "./Shop";
+import About from "./About";
+import Contact from "./Contact";
+import Cart from "./Cart";
 
 export default function App() {
   return (
     <>
       <BrowserRouter>
         <Navbar />
+        <Routes>
+          <Route path="/" element={<Home/>}></Route>
+          <Route path="/shop" element={<Shop/>}></Route>
+          <Route path="/About" element={<About/>}></Route>
+          <Route path="/Contact" element={<Contact/>}></Route>
+          <Route path="/Cart" element={<Cart/>}></Route>
+        </Routes>
+        <Footer />
       </BrowserRouter>
     </>
   );
